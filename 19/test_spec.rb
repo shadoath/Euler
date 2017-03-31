db197f20cf10db424389de3bf43aea91d6088c11
Skylar Bolton <sbolton@bcimedia.com>
2017-03-31 11:39:39 -0600

    Explaining specs on 19

--- 19/test_spec.rb
+++ 19/test_spec.rb
@@ -1,16 +1,17 @@
 require_relative "./problem.rb"
 RSpec.describe Number19 do
 
-  it "gets exmaple to validate" do
+  it "gets test to validate" do
     number = Number19.new('1900-01-01', '1901-01-01')
     expect(number.sunday_count).to eq 2
   end
 
-  it "gets answer to validate" do
+  it "gets The answer to validate" do
     number = Number19.new('1901-01-01', '1925-09-03')
     expect(number.sunday_count).to eq 42
   end
-  it "gets answer to validate" do
+
+  it "gets Euler answer to validate" do
     number = Number19.new('1901-01-01', '2000-12-31')
     expect(number.sunday_count).to eq 171
   end
